e4214278a8eecd3a73c47436a3fef2d881c6fc39
kladmax <coder.kladmax@gmail.com>
2019-06-09 16:17:04 +0300

    2.3 bugs

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -42,4 +42,17 @@ public class GreetingController {
 
         return "main";
    }
+
+   @PostMapping("filter")
+   public String filter(@RequestParam String filter, Map<String, Object> model) {
+       Iterable<Message> messages;
+       if (filter != null && filter.isEmpty() ) {
+           messages = messageRepo.findByTag(filter);
+       } else {
+           messages = messageRepo.findAll();
+       }
+       model.put("messages", messages);
+
+       return "main";
+   }
 }
